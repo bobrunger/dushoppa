c3a41a613c55e18c12bfc2ea4853181c344966a0
bobrunger <andreykoltsova@gmail.com>
2024-04-08 22:57:27 +0200

    fixed session cart counter

--- module/Application/src/Listener/LayoutListener.php
+++ module/Application/src/Listener/LayoutListener.php
@@ -13,16 +13,27 @@ class LayoutListener extends AbstractListenerAggregate
 {
     public function attach(EventManagerInterface $events, $priority = 1)
     {
-        $this->listeners[] = $events->attach(MvcEvent::EVENT_DISPATCH, [$this, 'onDispatch'], 100);
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_ROUTE, [$this, 'onDispatch'], 100);
     }
 
     public function onDispatch(MvcEvent $event)
     {
         $viewModel = $event->getViewModel();
         $miniCart = new Container('minicart');
+        $cartCount = new Container('cart');
+        $cartQty = 0;
         if (!isset($miniCart->items)) {
             $miniCart->items = [];
         }
+
+        if (!isset($cartCount->items)) {
+            $cartCount->items = [];
+        }
+        foreach ($cartCount->items as $id => $qty) {
+            $cartQty += $qty;
+        }
+
         $viewModel->setVariable('miniCartProducts', $miniCart->items); // cart products should be available in layout (header) as well as globally (eg about, contact us etc)
+        $viewModel->setVariable('cartCount', $cartQty);
     }
 }
